2b805c993bf1a025fa2bfa72d52bb556e5b61eb6
GOVARDAN.N.A <132485986+GOVARDAN-N-A@users.noreply.github.com>
2023-08-15 15:13:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,90 @@
+
+
+// document.addEventListener('DOMContentLoaded', function()  {
+//   console.log("DOM content loaded");
+//   const moreAboutBtn = document.getElementById('more-about-btn');
+//   const socialIcons = document.querySelector('.social-icons');
+//   const navbarToggle = document.querySelector('.navbar-toggle'); 
+//   const navbar = document.querySelector('.navbar');
+
+//   moreAboutBtn.addEventListener('click', () => {
+//       socialIcons.classList.toggle('active').style.overflow="hidden";
+//   });
+//   navbarToggle.addEventListener('click',() => {
+//   //  navbarToggle.style.display = "none";('active');
+
+//     navbar.style.display = 'block' ;
+//   });
+
+
+
+//   document.addEventListener('click', (event) => {
+//       const targetElement = event.target;
+
+//       if (!socialIcons.contains(targetElement) && targetElement !== moreAboutBtn) {
+//           socialIcons.classList.remove('active');
+//       }
+
+//       if (!navbar.contains(targetElement) && targetElement !== navbarToggle) {
+//           navbarToggle.display='block';
+//           navbar.classList.remove('active')
+//       }
+
+//       if (!navbar.contains(targetElement) && targetElement !== navbarToggle) {
+//         navbar.style.display = 'none';
+//       }
+
+//     //   if (!navbar.contains(targetElement) && targetElement !== navbarToggle) {
+//     //     navbarToggle.style.display ="block"
+//     // }
+     
+//   });
+//   console.log(navbar, navbarToggle)
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
-    const moreAboutBtn = document.getElementById('more-about-btn');
-    const socialIcons = document.querySelector('.social-icons');
-    const navbarToggle = document.getElementById('navbar-toggle');
-    const navbar = document.getElementById('navbar');
-  
-    moreAboutBtn.addEventListener('click', () => {
-      socialIcons.classList.toggle('active');
-    });
-  
-    document.addEventListener('click', (event) => {
-      const targetElement = event.target;
-  
-      if (!socialIcons.contains(targetElement) && targetElement !== moreAboutBtn) {
-        socialIcons.classList.remove('active');
-      }
-    });
-  
-    navbarToggle.addEventListener('click', () => {
-      navbar.classList.toggle('active');
-    });
-  
-    document.addEventListener('click', (event) => {
-      const targetElement = event.target;
-  
-      if (!navbar.contains(targetElement) && targetElement !== navbarToggle) {
-        navbar.classList.remove('active');
-      }
-    });
+  const moreAboutBtn = document.getElementById('more-about-btn');
+  const socialIcons = document.querySelector('.social-icons');
+  const navbarToggle = document.querySelector('.navbar-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  moreAboutBtn.addEventListener('click', () => {
+    socialIcons.classList.toggle('active').style.overflow = 'visible';
+    display.scr
   });
-  
+
+  navbarToggle.addEventListener('click', () => {
+    navbar.style.display = 'block';
+  });
+
+  // Function to handle window resize
+  function handleWindowResize() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth <= 932) {
+      navbarToggle.style.display = 'block';
+      navbar.style.display = 'none';
+    } else {
+      navbarToggle.style.display = 'none';
+      navbar.style.display = 'block';
+    }
+  }
+
+  // Initial call to handleWindowResize
+  handleWindowResize();
+
+  // Listen for window resize events
+  window.addEventListener('resize', handleWindowResize);
+
+  document.addEventListener('click', (event) => {
+    const targetElement = event.target;
+
+    if (!socialIcons.contains(targetElement) && targetElement !== moreAboutBtn) {
+      socialIcons.classList.remove('active');
+    }
+
+    if (!navbar.contains(targetElement) && targetElement !== navbarToggle) {
+        handleWindowResize()
+    }
+  });
+});
